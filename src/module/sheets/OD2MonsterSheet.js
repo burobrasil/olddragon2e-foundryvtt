bb0e9a45df3ef3d3260558fea3919326bc45b80f
Guilherme Mir <guilherme.mir@gmail.com>
2023-11-28 17:05:04 -0300

    dice rolls fix; add max load function; update monster sheet

--- src/module/sheets/OD2MonsterSheet.js
+++ src/module/sheets/OD2MonsterSheet.js
@@ -3,8 +3,8 @@ export default class OD2MonsterSheet extends ActorSheet {
     return mergeObject(super.defaultOptions, {
       template: 'systems/olddragon2e/templates/sheets/monster-sheet.hbs',
       classes: ['olddragon2e', 'sheet', 'monster'],
-      width: 820,
-      height: 780,
+      width: 600,
+      height: 650,
       tabs: [{ navSelector: '.tabs', contentSelector: '.section', initial: 'skills' }],
     });
   }
@@ -27,6 +27,7 @@ export default class OD2MonsterSheet extends ActorSheet {
     if (this.actor.isOwner) {
       html.find('.jp-check').click(this._onJPCheck.bind(this));
       html.find('.mo-check').click(this._onMOCheck.bind(this));
+      html.find('.dv-roll').click(this._onDVRoll.bind(this));
     }
 
     super.activateListeners(html);
@@ -313,6 +314,190 @@ export default class OD2MonsterSheet extends ActorSheet {
                 );
                 break;
             }
+          },
+        },
+        cancel: {
+          icon: "<i class='fa-solid fa-xmark'></i>",
+          label: 'Cancelar',
+        },
+      },
+      default: 'roll',
+    }).render(true);
+  }
+
+  // Rolagem de Dado de Vida (DV)
+  async _onDVRoll(event) {
+    event.preventDefault();
+    const monsterName = this.actor.name;
+    const dvInput = this.actor.system.dv;
+    const dvBonusInput = this.actor.system.dv_bonus;
+    let dvBonus = '';
+    let currentHpInput = document.querySelector('[name="system.hp.value"]');
+    let maxHpInput = document.querySelector('[name="system.hp.max"]');
+    let diceQuantity = 1;
+    let formula = `${diceQuantity}d8`;
+
+    if (dvInput && dvInput.trim() !== '') {
+      const dvValue = dvInput.trim();
+
+      if (!isNaN(dvValue) && Number.isInteger(parseFloat(dvValue))) {
+        diceQuantity = dvValue;
+        formula = `${diceQuantity}d8`;
+      } else if (dvValue === '½') {
+        formula = '1d8';
+      } else if (dvValue.includes('a')) {
+        const rangeValues = dvValue.split('a').map((val) => parseInt(val.trim()));
+        if (rangeValues.length === 2 && !isNaN(rangeValues[0]) && !isNaN(rangeValues[1])) {
+          const min = rangeValues[0];
+          const max = rangeValues[1];
+          diceQuantity = Math.round((min + max) / 2);
+          formula = `${diceQuantity}d8`;
+        }
+      }
+    }
+
+    if (dvBonusInput !== '' && dvBonusInput !== '0') {
+      dvBonus = dvBonusInput;
+    }
+
+    new Dialog({
+      title: `Rolar Dado de Vida`,
+      content: `
+                <div class="form-group">
+                    <label>Fórmula</label>
+                    <input type="text" name="formula" value="${formula}" disabled>
+                </div>
+                <div class="form-group">
+                    <label>Mod. Opcional <em>(valor ou dados)</em></label>
+                    <input type="text" name="bonus" id="bonus" value="" placeholder="Ex.: -1, 10, 2d4, 1d6 + 1d8 + 1d10">
+                </div>
+                <div class="form-group">
+                    <label>Modo de Rolagem</label>
+                    <select name="rollMode" id="rollMode">
+                        <option value="public" selected>Public Roll</option>
+                        <option value="private">Private GM Roll</option>
+                        <option value="blind">Blind GM Roll</option>
+                        <option value="self">Self Roll</option>
+                    </select>
+                </div>
+            `,
+      buttons: {
+        roll: {
+          icon: "<i class='fa-solid fa-dice-d20'></i>",
+          label: 'Rolar',
+          callback: async (html) => {
+            const dvInput = this.actor.system.dv;
+            const bonus = html.find('#bonus').val();
+            const mode = html.find('#rollMode').val();
+            let rollFormula = formula;
+
+            if (bonus) {
+              rollFormula += `+${bonus}`;
+            }
+
+            let rollResult;
+            let hp;
+
+            if (dvInput === '½') {
+              rollResult = await new Roll('1d8').roll({ async: true });
+              hp = Math.ceil(rollResult.total / 2);
+            } else {
+              rollResult = await new Roll(rollFormula).roll({ async: true });
+              hp = rollResult.total;
+            }
+
+            if (dvBonus) {
+              hp += parseInt(dvBonus || 0);
+            }
+
+            let bonusText = '';
+
+            if (dvInput === '½' && dvBonus) {
+              bonusText = ` + ${parseInt(dvBonus)} <em>(DV Bônus)</em> = <strong>${
+                Math.ceil(rollResult.total / 2) + parseInt(dvBonus)
+              }</strong>`;
+            }
+
+            if (dvInput !== '½' && dvBonus) {
+              bonusText = ` + ${parseInt(dvBonus)} <em>(DV Bônus)</em> = <strong>${
+                rollResult.total + parseInt(dvBonus)
+              }</strong>`;
+            }
+
+            let resultText = '';
+
+            if (dvInput === '½' && dvBonus) {
+              resultText = `<h2 class='text-center'>Rolagem de <strong>Pontos de Vida</strong></h2><p class='text-center'>PV Totais: ${
+                rollResult.total
+              } &div; 2 &cong; ${Math.ceil(rollResult.total / 2)}${bonusText}</p>`;
+            }
+
+            if (dvInput === '½' && !dvBonus) {
+              resultText = `<h2 class='text-center'>Rolagem de <strong>Pontos de Vida</strong></h2><p class='text-center'>PV Totais: ${
+                rollResult.total
+              } &div; 2 &cong; ${Math.ceil(rollResult.total / 2)}</p>`;
+            }
+
+            if (dvInput !== '½' && dvBonus) {
+              resultText = `<h2 class='text-center'>Rolagem de <strong>Pontos de Vida</strong></h2><p class='text-center'>PV Totais: ${rollResult.total}${bonusText}</p>`;
+            }
+
+            if (dvInput !== '½' && !dvBonus) {
+              resultText = `<h2 class='text-center'>Rolagem de <strong>Pontos de Vida</strong></h2><p class='text-center'>PV Totais: ${rollResult.total}</p>`;
+            }
+
+            switch (mode) {
+              case 'private':
+                rollResult.toMessage(
+                  {
+                    flavor: resultText,
+                    speaker: { alias: `${this.truncateString(monsterName, 30)}` },
+                    whisper: [game.user],
+                  },
+                  { rollMode: CONST.DICE_ROLL_MODES.PRIVATE },
+                );
+                break;
+              case 'blind':
+                rollResult.toMessage(
+                  {
+                    flavor: resultText,
+                    speaker: { alias: `${this.truncateString(monsterName, 30)}` },
+                    whisper: [game.user],
+                  },
+                  { rollMode: CONST.DICE_ROLL_MODES.BLIND },
+                );
+                break;
+              case 'self':
+                rollResult.toMessage(
+                  {
+                    flavor: resultText,
+                    speaker: { alias: `${this.truncateString(monsterName, 30)}` },
+                    whisper: [game.user],
+                  },
+                  { rollMode: CONST.DICE_ROLL_MODES.SELF },
+                );
+                break;
+              case 'public':
+              default:
+                rollResult.toMessage(
+                  {
+                    flavor: resultText,
+                    speaker: { alias: `${this.truncateString(monsterName, 30)}` },
+                    whisper: [game.user],
+                  },
+                  { rollMode: CONST.DICE_ROLL_MODES.PUBLIC },
+                );
+                break;
+            }
+
+            currentHpInput.value = hp;
+            maxHpInput.value = hp;
+
+            const updateObject = {};
+            updateObject['system.hp.value'] = hp;
+            updateObject['system.hp.max'] = hp;
+
+            await this.document.update(updateObject);
           },
         },
         cancel: {
