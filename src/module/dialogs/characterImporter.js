7bedfd78ca11d7df6932c160ed447d1cad3c4b0b
Samuel Grave <samuel@dinie.com>
2023-11-29 14:24:22 -0300

    :sparkles: Add buttom to import characters from ODO json

---
+++ src/module/dialogs/characterImporter.js
@@ -0,0 +1,53 @@
+import { importActor } from '../api/characterImporter';
+
+class CharacterImporterDialog extends Application {
+  constructor(options = {}) {
+    super(options);
+  }
+
+  /** @override */
+  static get defaultOptions() {
+    const options = super.defaultOptions;
+    options.id = 'character-importer-dialog';
+    options.title = 'Importar Personagem';
+    options.template = 'systems/olddragon2e/templates/dialog/character-importer-dialog.hbs';
+    options.width = 420;
+    options.height = 'auto';
+    return options;
+  }
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+    html.find('.cancel-button').on('click', this._onCancel.bind(this));
+    html.find('.character-importer-button').on('click', this._onCharacterImporter.bind(this));
+  }
+
+  async _onCancel(event) {
+    event.preventDefault();
+    await this.close();
+  }
+
+  async _onCharacterImporter(event) {
+    event.preventDefault();
+    console.log('on _onCharacterImporter');
+    const json = document.querySelector('#character-importer-textarea').value;
+
+    if (json === '') return;
+
+    try {
+      const actor = await importActor(JSON.parse(json));
+      actor.sheet.render(true);
+
+      await this.close();
+    } catch (err) {
+      console.error(err);
+      ui.notifications.error(`Error importing character. Check console for error log.`);
+    }
+  }
+}
+
+export const showCharacterImporter = () => {
+  const characterImporterDialog = new CharacterImporterDialog();
+  characterImporterDialog.render(true);
+};
